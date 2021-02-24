6a965dcd2f9f2e65fbe310e9b5a7e4574e8a9248
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-02-24 16:35:15 -0300

    Importa componentes criados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,30 @@
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 
 import Head from "next/head";
 
+import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="container">
-    <ExperienceBar />
-  </div>
+    <div className={styles.container}>
       <Head>
         <title>Início | move.it</title>
       </Head>
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div>
+
+        </div>
+      </section>
+    </div>
   )
 }
