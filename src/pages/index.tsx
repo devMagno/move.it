19c0847171069c947b0c991f1133e44b70c3e43d
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-02-24 16:11:52 -0300

    Adiciona título e favicon da página

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import { ExperienceBar } from "../components/ExperienceBar";
 
+import Head from "next/head";
+
+
 export default function Home() {
   return (
     <div className="container">
     <ExperienceBar />
   </div>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
   )
 }
