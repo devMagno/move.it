4d9764a6206395cd5d24c7c630a8ea767505384b
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-02-27 19:41:17 -0300

    Adiciona componente ChallengeBox

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,32 @@
+import styles from '../styles/components/ChallengeBox.module.css'
+
+export function ChallengeBox() {
+  const hasActiveChallenge = true;
+
+  return(
+    <div className={styles.challengeBoxContainer}>
+      { hasActiveChallenge ? (
+        <div className={styles.challengeActive}>
+          <header>Ganhe 400 xp</header>
+          <main>
+            <img src="icons/body.svg" alt=""/>
+            <strong>Novo Desafio</strong>
+            <p>Levante e faça uma caminhada de 3 minutos.</p>
+          </main>
+          <footer>
+            <button type="button" className={styles.challengeFailedButton}>Falhei</button>
+            <button type="button" className={styles.challengeSucceededButton}>Completei</button>
+          </footer>
+        </div>
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>Finalize um ciclo para receber um desafio</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Level Up"/>
+            Avance de level completando desafios.
+          </p>
+        </div>
+      )}
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import Head from "next/head";
 
@@ -22,7 +23,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
